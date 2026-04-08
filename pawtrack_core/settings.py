cb0217aea640ea81c77f2cab9301195bee5bc8af
PaulPasumala <pasumalapaul384@gmail.com>
2026-04-09 00:59:41 +0800

    New Database SQLite3

--- pawtrack_core/settings.py
+++ pawtrack_core/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'pawtrack_core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'pawtrack$defaultdb',        
-        'USER': 'pawtrack',                 
-        'PASSWORD': '09458356869Pasumala', 
-        'HOST': 'pawtrack.mysql.pythonanywhere-services.com',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
